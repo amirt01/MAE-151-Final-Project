7b2e82362a1817ac0e7d0c77f0da272b5dd5ce11
amir tadros <amirtadros01@gmail.com>
2023-03-19 19:38:10 -0700

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@
 
 #define ANGLE_SERVO_PIN 11
 Servo angle_servo;
-const int MAX_LAUNCH_ANGLE = 35;
-const int MIN_LAUNCH_ANGLE = 5;
-const int MAX_SERVO_ANGLE = 0;
-const int MIN_SERVO_ANGLE = 180;
+constexpr short MAX_LAUNCH_ANGLE = 35;
+constexpr short MIN_LAUNCH_ANGLE = 5;
+constexpr short MAX_SERVO_ANGLE = 0;
+constexpr short MIN_SERVO_ANGLE = 180;
 double desired_launch_angle = 0.0;
 double desired_servo_angle = 0.0;
 
@@ -23,8 +23,8 @@ double desired_servo_angle = 0.0;
 
 #define TRIGGER_SERVO_PIN 10
 Servo trigger_servo;
-const int TRIGGER_CLOSED_ANGLE = 80;
-const int TRIGGER_OPEN_ANGLE = 90;
+constexpr int TRIGGER_CLOSED_ANGLE = 80;
+constexpr int TRIGGER_OPEN_ANGLE = 90;
 
 // ================================================================
 // ===                      MPU VARIABLES                       ===
@@ -33,7 +33,7 @@ const int TRIGGER_OPEN_ANGLE = 90;
 Adafruit_MPU6050 mpu;
 void MPU_Read();
 
-const int RECORDING_TIME = 250;
+constexpr int RECORDING_TIME = 250;
 unsigned long launch_start_time = 0;
 unsigned long launch_end_time = 0;
 
@@ -45,18 +45,17 @@ float actual_launch_angle = radians(0);  // FIXME: add initial launch angle
 // ===                       OPERATIONS                         ===
 // ================================================================
 
-const unsigned long LAUNCH_DELAY = 3000;
+constexpr unsigned int LAUNCH_DELAY = 3000;
 
-// #define LAUNCH_BUTTON_PIN 4
 #define LED_PIN 13
 bool blinkState = false;
 unsigned long toggle_time = 0;
-const unsigned int LED_INCREMENT = 300;
+constexpr unsigned int LED_INCREMENT = 300;
 
 #define BUZZER_PIN 9
 bool buzzer_state = false;
 unsigned long buzzer_time = 0;
-const unsigned int BUZZER_INCREMENT = LAUNCH_DELAY / 3 / 2;
+constexpr unsigned int BUZZER_INCREMENT = LAUNCH_DELAY / 3 / 2;
 
 enum class State { StandBye, Angle_Adjusting, Launching, Resting } state;
 
@@ -79,9 +78,7 @@ void Print_Data();
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) {
-    delay(10); // will pause until serial console opens
-  }
+  while (!Serial) delay(10); // will pause until serial console opens
   Serial.println("Serial Initialized");
 
   MPU_Setup();
@@ -110,9 +107,9 @@ void loop() {
       else if (desired_servo_angle < MIN_SERVO_ANGLE) angle_servo.write(MIN_SERVO_ANGLE);
       else angle_servo.write(desired_servo_angle);
 
-      launch_start_time = millis() + LAUNCH_DELAY;
-
       state = State::Angle_Adjusting;
+
+      launch_start_time = millis() + LAUNCH_DELAY;
 
     case State::Angle_Adjusting:
       // buzz until launch timer goes off
@@ -120,14 +117,14 @@ void loop() {
         Update_Buzzer();
         break;
       }
-      
-      // Track launch time
-      launch_end_time = millis() + RECORDING_TIME;
+
+      state = State::Launching;
 
       // Update trigger servo
       trigger_servo.write(TRIGGER_OPEN_ANGLE);
 
-      state = State::Launching;
+      // Track launch time
+      launch_end_time = millis() + RECORDING_TIME;
 
     case State::Launching:
       // Read until launch is over
@@ -135,10 +132,10 @@ void loop() {
         MPU_Read();
         break;
       }
-      
-      Print_Data();
 
       state = State::Resting;
+
+      Print_Data();
 
     case State::Resting:
       noTone(BUZZER_PIN);
@@ -155,9 +152,7 @@ void loop() {
 void MPU_Setup() {
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
+    while (true) delay(10);
   }
   
   mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
@@ -168,16 +163,14 @@ void MPU_Setup() {
 void Servo_Setup() {
   trigger_servo.attach(TRIGGER_SERVO_PIN);
   trigger_servo.write(TRIGGER_CLOSED_ANGLE);
+
   angle_servo.attach(ANGLE_SERVO_PIN);
   angle_servo.write(MIN_SERVO_ANGLE);
+
   Serial.println("Servos Initialized");
 }
 
 void Operations_Setup() {
-  // TODO: Add on board button support
-  // pinMode(LAUNCH_BUTTON_PIN, INPUT);
-  // attachInterrupt(digitalPinToInterrupt(LAUNCH_BUTTON_PIN), ActivateTrigger, FALLING);
-
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, blinkState);
   toggle_time = millis() + 1000;
@@ -185,6 +178,8 @@ void Operations_Setup() {
   pinMode(BUZZER_PIN, OUTPUT);
 
   state = State::StandBye;
+
+  Serial.println("Operations Initialized");
 }
 
 void MPU_Read() {
@@ -200,7 +195,7 @@ void MPU_Read() {
   // Update launch_angle
   actual_launch_angle += -g.gyro.y * (millis() - last_pos_time) / 1000;  // FIXME: bug here
 
-  // Print velocity
+  // Print angular velocity
   Serial.print(millis());
   Serial.print(" ");
   Serial.println(-g.gyro.y);
@@ -209,16 +204,22 @@ void MPU_Read() {
 // Blink every second
 void Update_Blink() {
   if (millis() < toggle_time) return;
+
+  // Update LED
   blinkState = !blinkState;
-  toggle_time = millis() + LED_INCREMENT;
   digitalWrite(LED_PIN, blinkState);
+
+  toggle_time = millis() + LED_INCREMENT;
 }
 
 void Update_Buzzer() {
   if (millis() < buzzer_time) return;
+  
+  // Update buzzer
   buzzer_state = !buzzer_state;
-  if (buzzer_state) tone(BUZZER_PIN, 1000);
+  if (buzzer_state) tone(BUZZER_PIN, 1000);  // 1000Hz
   else noTone(BUZZER_PIN);
+
   buzzer_time += BUZZER_INCREMENT;
 }
 
